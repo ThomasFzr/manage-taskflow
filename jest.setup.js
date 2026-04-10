3cfe2782379a17130964a28b82c9aa06fdf027ec
ThomasFzr <thfltzr@gmail.com>
2026-04-10 10:14:12 +0200

    Add Jest mock for axios and update axios version to 1.15.0

--- jest.setup.js
+++ jest.setup.js
@@ -3,6 +3,19 @@ process.env.EXPO_PUBLIC_APP_VERSION = '1.0.0';
 process.env.EXPO_PUBLIC_ENVIRONMENT = 'development';
 process.env.EXPO_PUBLIC_API_URL = 'https://api.test.com';
 
+jest.mock('axios', () => {
+  const instance = {
+    get: jest.fn(),
+    post: jest.fn(),
+    patch: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  return {
+    create: jest.fn(() => instance),
+  };
+});
+
 jest.mock('@react-native-community/datetimepicker', () => {
   const React = require('react');
   const { View } = require('react-native');
